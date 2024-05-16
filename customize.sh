3dcd2c0634ab3b182c989870d70cdf0356a0b204
liugaoshuo <58382337+liugaoshuo@users.noreply.github.com>
2024-05-16 18:29:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,5 +11,7 @@ sed -i 's/192.168.1.1/192.168.31.2/g' openwrt/package/base-files/files/bin/confi
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
+#清华源
+sed -i 's_downloads.openwrt.org_mirrors.tuna.tsinghua.edu.cn/openwrt_' /etc/opkg/distfeeds.conf
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
